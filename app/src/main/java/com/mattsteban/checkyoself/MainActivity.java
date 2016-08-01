a252f1ffa2b632a193c78aff5586c2173a62b7a2
Esteban Selaya <Esteban.Selaya@hart.com>
2016-08-01 19:20:28 -0400

    no message

--- app/src/main/java/com/mattsteban/checkyoself/MainActivity.java
+++ app/src/main/java/com/mattsteban/checkyoself/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mattsteban.checkyoself;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,32 +17,87 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mattsteban.checkyoself.models.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    FirebaseAuth auth;
 
+    @BindView(R.id.current_logged_in_user_email)
+    TextView tvCurrentLoggedInEmail;
+
+    @BindView(R.id.btn_trigger_user_call)
+    Button btnTriggerUserCall;
+
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        auth = FirebaseAuth.getInstance();
+        ButterKnife.bind(this);
 
-        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference dbRefUsers = database.getReference("users");
+
+
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
-                    Toast.makeText(getApplicationContext(), "User Signed In!", Toast.LENGTH_SHORT).show();
+
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getToken() instead.
+                    String uid = user.getUid();
+
+                    tvCurrentLoggedInEmail.setText(email);
+
+                    dbRefUsers.setValue(new User(email, name, email, true, photoUrl != null ? photoUrl.toString() : "SOME_URL.com"));
                 } else {
                     // User is signed out
-                    Toast.makeText(getApplicationContext(), "User Not In!", Toast.LENGTH_SHORT).show();
+                    tvCurrentLoggedInEmail.setText("Not Currently Signed In.");
                 }
             }
-        };
+        });
+    }
+
+    private void writeNewUser(){
+
+    }
+
+    @OnClick(R.id.btn_trigger_user_call)
+    public void onBtnTriggerUserCallClick(View view){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+            tvCurrentLoggedInEmail.setText(email);
+        }
+        else {
+            tvCurrentLoggedInEmail.setText("Not Currently Signed In.");
+        }
     }
 
     // Options Menu Selection. Sign In or Sign Out.
